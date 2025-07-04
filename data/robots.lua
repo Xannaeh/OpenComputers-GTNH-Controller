2c8667156db7019c16e57d49663876c5d827bb31
Xannaeh <44808157+Xannaeh@users.noreply.github.com>
2025-07-04 18:25:10 +0200

    Store robots in Lua-serialised table

---
+++ data/robots.lua
@@ -0,0 +1,17 @@
+-- return a Lua table so dofile() gives it directly
+return {
+  robots = {
+    {
+      id      = "robot_001",
+      jobType = "courier",
+      status  = "idle",
+      active  = true
+    },
+    {
+      id      = "robot_002",
+      jobType = "janitor",
+      status  = "idle",
+      active  = false
+    }
+  }
+}
