8dada3fa9eadc2de9bc460a07ca16460a3234f37
Xannaeh <44808157+Xannaeh@users.noreply.github.com>
2025-07-04 13:08:37 +0200

    Fix install.lua to use os.execute

--- install.lua
+++ install.lua
@@ -1,36 +1,21 @@
--- Create folders
-mkdir /apps
-mkdir /apps/launcher
-mkdir /apps/fleet
-mkdir /apps/power
-mkdir /apps/net
-mkdir /apps/robot_agent
-mkdir /data
+os.execute("mkdir /apps")
+os.execute("mkdir /apps/launcher")
+os.execute("mkdir /apps/fleet")
+os.execute("mkdir /apps/power")
+os.execute("mkdir /apps/net")
+os.execute("mkdir /apps/robot_agent")
+os.execute("mkdir /data")
 
--- Download boot.lua
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/boot.lua /boot.lua
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/boot.lua /boot.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/launcher/launcher.lua /apps/launcher/launcher.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/fleet/fleet.lua /apps/fleet/fleet.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/fleet/Job.lua /apps/fleet/Job.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/fleet/Robot.lua /apps/fleet/Robot.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/power/power.lua /apps/power/power.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/net/net.lua /apps/net/net.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/robot_agent/robot_agent.lua /apps/robot_agent/robot_agent.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/data/config.json /data/config.json")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/data/recipes.json /data/recipes.json")
+os.execute("wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/data/machines.json /data/machines.json")
 
--- Download launcher
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/launcher/launcher.lua /apps/launcher/launcher.lua
-
--- Download fleet files
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/fleet/fleet.lua /apps/fleet/fleet.lua
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/fleet/Job.lua /apps/fleet/Job.lua
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/fleet/Robot.lua /apps/fleet/Robot.lua
-
--- Download power manager
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/power/power.lua /apps/power/power.lua
-
--- Download net manager
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/net/net.lua /apps/net/net.lua
-
--- Download robot agent
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/apps/robot_agent/robot_agent.lua /apps/robot_agent/robot_agent.lua
-
--- Download empty data files (optional)
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/data/config.json /data/config.json
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/data/recipes.json /data/recipes.json
-wget -f https://raw.githubusercontent.com/Xannaeh/OpenComputers-GTNH-Controller/main/data/machines.json /data/machines.json
-
--- Done! Now run:
-boot.lua
+print("Done! Now run: boot.lua")
