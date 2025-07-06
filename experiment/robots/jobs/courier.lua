33aff20f6026c98919a30d7f4c6c9fd21741227f
Xannaeh <44808157+Xannaeh@users.noreply.github.com>
2025-07-06 19:32:11 +0200

    Add basic movement and item transfer logic to Courier job

--- experiment/robots/jobs/courier.lua
+++ experiment/robots/jobs/courier.lua
@@ -1,13 +1,41 @@
 -- courier.lua
 -- Courier Job: moves items between locations
 
-local Job = require("job")
+local component = require("component")
+local robot = require("robot")
+local sides = require("sides")
 
+local Job = require("job")
 local Courier = Job:new()
 
 function Courier:execute()
     print("Courier job started.")
-    -- Add your movement & transfer logic here
+
+    -- Example: move forward 3 blocks, grab items, come back
+    for i = 1, 3 do
+        robot.forward()
+    end
+
+    -- Try to suck items from a chest in front
+    if robot.suck() then
+        print("Items collected.")
+    else
+        print("Nothing to collect.")
+    end
+
+    -- Return to start
+    for i = 1, 3 do
+        robot.back()
+    end
+
+    -- Drop items behind
+    if robot.drop(sides.back) then
+        print("Items dropped.")
+    else
+        print("Nothing to drop.")
+    end
+
+    print("Courier job done.")
 end
 
 return Courier
