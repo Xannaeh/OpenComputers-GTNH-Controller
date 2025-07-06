37c9b91172eb33222bfcc05d3e25116e771b8fe8
Xannaeh <44808157+Xannaeh@users.noreply.github.com>
2025-07-06 19:37:00 +0200

    Fix Courier job: turn around to drop in target chest properly

--- experiment/robots/jobs/courier.lua
+++ experiment/robots/jobs/courier.lua
@@ -3,7 +3,6 @@
 
 local component = require("component")
 local robot = require("robot")
-local sides = require("sides")
 
 local Job = require("job")
 local Courier = Job:new()
@@ -11,29 +10,50 @@ local Courier = Job:new()
 function Courier:execute()
     print("Courier job started.")
 
-    -- Example: move forward 3 blocks, grab items, come back
+    -- Move to pickup chest
     for i = 1, 3 do
         robot.forward()
     end
 
-    -- Try to suck items from a chest in front
+    -- Pick up items
     if robot.suck() then
         print("Items collected.")
     else
         print("Nothing to collect.")
     end
 
-    -- Return to start
+    -- Return to base
     for i = 1, 3 do
         robot.back()
     end
 
-    -- Drop items behind
-    if robot.drop(sides.back) then
+    -- Turn 180° to face drop-off chest
+    robot.turnAround = function()
+        robot.turnLeft()
+        robot.turnLeft()
+    end
+
+    robot.turnAround()
+
+    -- Move to drop-off chest (3 blocks forward in opposite direction)
+    for i = 1, 3 do
+        robot.forward()
+    end
+
+    -- Drop items
+    if robot.drop() then
         print("Items dropped.")
     else
         print("Nothing to drop.")
     end
+
+    -- Return to base
+    for i = 1, 3 do
+        robot.back()
+    end
+
+    -- Turn back to original orientation
+    robot.turnAround()
 
     print("Courier job done.")
 end
